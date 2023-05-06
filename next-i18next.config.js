ad7c305e0fd4652ee6ffd89f0c4296fcc4e79e07
Oded Winberger <odedindi@gmail.com>
2023-05-06 11:04:34 +0200

    fix next-i18next.config localePath

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,13 +1,10 @@
-const cwd = require('process').cwd
-const path = require('path')
-
 module.exports = {
 	debug: process.env.NODE_ENV === 'development',
 	i18n: {
 		defaultLocale: 'en',
 		locales: ['en', 'de', 'he', 'fa-IR'],
 	},
-	localePath: path.join(cwd(), 'src', 'config', 'locales'),
+	localePath: 'src/config/locales',
 	ns: ['common'],
 	rtlLocales: ['he', 'fa-IR'],
 }
